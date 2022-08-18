ef35bc721e3a423f6c55cd226e1bedf264c347a1
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2022-08-17 22:31:39 -0500

    fix crossOrigin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,15 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Antonio:wght@100&family=Notable&display=swap"
           rel="stylesheet"
+          crossOrigin=""
         />
       </Head>
       <body>
