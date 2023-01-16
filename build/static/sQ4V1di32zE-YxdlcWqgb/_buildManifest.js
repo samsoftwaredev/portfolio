09a8b8d44fbcefafac8b054a6f77833d4a71abdc
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2023-01-16 15:11:04 -0600

    removed build folder from gitignore

---
+++ build/static/sQ4V1di32zE-YxdlcWqgb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/13-f8c9c0d1650bffc5.js","static/css/50919f5831eec557.css","static/chunks/pages/index-638983aa8e784526.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
