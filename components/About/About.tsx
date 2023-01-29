20598608be188b3d7035185bbdd38b849151e83f
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2023-01-29 12:28:14 -0600

    improve bio from about page

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -33,6 +33,30 @@ const About = () => {
             With years of strong academic performance and hands-on programming
             experience, my purpose is to make an impact by improving
             people&apos;s lives through the use of tech.
+            <br />
+            I have worked as a front-end developer for a couple of years now. I
+            have seen it all. Everything from optimization to accessibility,
+            testing and writing efficient algorithms.
+            <br />
+            <br />
+            I have worked in multiple startups and I have had the opportunity to
+            build my own startup. While I was in college, studying Computer
+            Science I was also developing AlliesPro, my first startup.
+            <br />
+            <br />
+            AlliesPro is the world&apos;s first Pay Per File System where savvy
+            solo professionals and freelancers get paid on time! Thanks to this
+            side project, which later became a startup, I was featured in the
+            CALIFORNIA (CAL) STATE UNIVERSITY EAST BAY newspaper.
+            <br />
+            While studying, I also worked full-time on my startup, sacrificing
+            weekends and holidays for the success of the company. That&apos;s
+            how passionate I&apos;m about building apps that can have a positive
+            impact on society.
+            <br />
+            <br />
+            I graduated with a B.S. in Computer Science from California State
+            University East Bay in 2019.
             <SocialMedia />
           </Typography>
         </Box>
